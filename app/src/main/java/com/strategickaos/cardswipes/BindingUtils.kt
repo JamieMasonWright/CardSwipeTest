c399c5cf29c499b0a345841de5abcbb35ed6264d
Jamie Mason_Wright <jamie.mason-wright@lush.co.uk>
2020-05-27 11:50:27 +0100

    Initial commit

---
+++ app/src/main/java/com/strategickaos/cardswipes/BindingUtils.kt
@@ -0,0 +1,9 @@
+package com.strategickaos.cardswipes
+
+import androidx.databinding.BindingAdapter
+import com.facebook.drawee.view.SimpleDraweeView
+
+@BindingAdapter("image")
+fun loadImage(view: SimpleDraweeView, url: String) {
+    view.setImageURI(url)
+}
